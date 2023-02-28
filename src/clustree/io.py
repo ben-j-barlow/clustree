5c470506d939fe2d945708402368cc9ed75c105b
Ben Barlow <ben-j-barlow.1@gmail.com>
2023-02-28 16:23:00 +0000

    chore: re-add fake image creation to repo

--- src/clustree/io.py
+++ src/clustree/io.py
@@ -3,8 +3,28 @@ from pathlib import Path
 from typing import List, Union
 
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 
 from clustree.clustree_typing import IMAGE_CONFIG_TYPE
+
+
+def get_fake_img(k_upper: str, k_lower: str, w: int = 40, h: int = 40) -> np.ndarray:
+    """
+    Create fake image reading 'K_k'.
+    :param k_upper: cluster resolution
+    :param k_lower: cluster number
+    :param w: number of pixels
+    :param h: number of pixels
+    :return: image with white background and black text
+    """
+    img = Image.new("RGB", (w, h), color=(255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.load_default()
+    out = f"{k_upper}_{k_lower}"
+    _, _, i, j = draw.textbbox((0, 0), out, font=font)
+    draw.text(((w - i) / 2, (h - j) / 2), out, font=font, fill="black")
+    return np.asarray(img)
 
 
 def read_images(
